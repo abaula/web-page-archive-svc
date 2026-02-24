b33534ff7e8c9bdc11320801d363d9c225f65b82
A.Baula <anton.baula@gmail.com>
2026-02-24 15:43:12 +0000

    Work.

---
+++ src/WebPageArchiveClient/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using WebPageArchive.GprsProto;
+
+var channel = GrpcChannel.ForAddress("http://localhost:8000"); // ваш порт сервера
+var client = new PageDownloader.PageDownloaderClient(channel);
+
+var request = new DownloadRequest { Url = "https://ixbt.com" };
+var response = await client.DownloadPageAsync(request);
+var zipBytes = response.ZipArchive.ToByteArray();
+// Пишем на диск
+var path = Path.Combine(Path.GetTempPath(), "ixbt_test.zip");
+await File.WriteAllBytesAsync(path, zipBytes);
+
+Console.WriteLine($"Response: {response.OriginalUrl} to file {path}");
