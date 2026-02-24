5a7e82e9a236d5126e8834f939608f9f6f456096
A.Baula <anton.baula@gmail.com>
2026-02-24 11:46:39 +0000

    Work.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # web-page-archive-svc
 A microservice for loading a web page at a specified URL and converting it to MHTML format.
+
+
+# Install Playwright
+
+```bash
+# dotnet add package Microsoft.Playwright # уже добавлен в проект
+dotnet build
+```
+
+## install powershell
+```bash
+# пример для Ubuntu
+sudo apt-get update
+sudo apt-get install -y powershell
+```
+
+## install browsers headless only
+```bash
+pwsh bin/Debug/net8.0/playwright.ps1 install --with-deps --only-shell
+```
---
+++ src/WebPageArchive/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+
+namespace WebPageArchive;
+
+internal class Program
+{
+    public static async Task Main()
+    {
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = true
+        });
+        await using var context = await browser.NewContextAsync();
+        var page = await context.NewPageAsync();
+        var gotoOptions = new PageGotoOptions
+        {
+            WaitUntil = WaitUntilState.DOMContentLoaded,
+            Timeout = 60000
+        };
+
+        await page.GotoAsync("https://ixbt.com", gotoOptions);
+        /*
+        await page.ScreenshotAsync(new PageScreenshotOptions
+        {
+            Path = "screenshot.png",
+            FullPage = true
+        });
+        */
+
+        // Создаём CDP‑сессию для страницы
+        await using var client = await context.NewCDPSessionAsync(page);   // аналог new_cdp_session в Python/JS [web:61]
+
+        // Вызываем Page.captureSnapshot, формат по умолчанию = "mhtml" [web:56][web:63]
+        var result = await client.SendAsync("Page.captureSnapshot");
+
+        // В result лежит словарь, вытаскиваем поле "data"
+        if (result != null)
+        {
+            // гарантируем ненулевую строку
+            var jsonString = result.ToString()!;
+            var mhtmlJson = JsonDocument.Parse(jsonString);
+            foreach(var el in mhtmlJson.RootElement.EnumerateObject())
+            {
+                var a = el;
+            }
+
+
+            var mhtml = mhtmlJson.RootElement.GetProperty("data").GetString();
+
+            await File.WriteAllTextAsync("page.mhtml", mhtml ?? "");
+
+            Console.WriteLine("Saved to page.mhtml");
+        }
+
+        await page.CloseAsync();
+    }
+}
